f219075cfb1753ae8fb8af4274b85d19bb8a0833
Jens Gwen Stein <jens.s.stein@gmail.com>
2022-01-08 18:49:45 +0100

    Move test utilities to a separate crate
    To be able to access them from different tests, both unit tests and
    integration tests.

--- src/test_main.rs
+++ src/test_main.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-use crate::test_utils::create_db;
+use test_utils::create_db;
 
 use sqlx::Row;
 
@@ -37,6 +37,7 @@ fn test_traverse_document() {
 #[tokio::test]
 async fn test_database_migration() {
     let conn = create_db().await;
+    migrate_db(&conn).await.expect("Unable to migrate database");
     sqlx::query("INSERT INTO users VALUES(1, 'user', '$argon2id$v=19$m=4096,t=3,p=1$ewSM8Hmctto5QHVv27S1cA$o6GeMd3PriFhi2CalkBmG1cV/AMi+ry0r/6fjmeSaFQ')")
         .execute(&conn)
         .await.expect("Unable to insert into database");
