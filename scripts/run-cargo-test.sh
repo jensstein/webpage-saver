57e34a38445993468f37bfc8a9d5e97c3ebbd5b4
Jens Gwen Stein <jens.s.stein@gmail.com>
2022-07-12 22:55:35 +0200

    Test if test database is ready for use

--- scripts/run-cargo-test.sh
+++ scripts/run-cargo-test.sh
@@ -8,8 +8,13 @@ NAME=woom
 PORT=$(python3 -c 'import socket; s = socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 
 POD_ID=$(podman create --replace --rm --name $NAME-test -e POSTGRES_DB=$NAME-test -e POSTGRES_PASSWORD=$NAME-test -e POSTGRES_USER=$NAME-test -p $PORT:5432 postgres:14-alpine)
-podman start $POD_ID
+podman start $NAME-test
+set +x
+while true; do
+	podman exec $NAME-test psql -U $NAME-test -d $NAME-test -c "select 1" && break || true
+done
+set -x
 
-trap "podman stop $POD_ID" EXIT
+trap "podman stop $NAME-test" EXIT
 
 TEST_DB=postgresql://$NAME-test:$NAME-test@localhost:$PORT cargo test $@ || true
