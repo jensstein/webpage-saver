faa0866ea51bb9fe2d7d36597e378a4462337417
Jens Gwen Stein <jens.s.stein@gmail.com>
2022-07-16 22:45:12 +0200

    Add helper for handling api requests
    This helpers exists mainly for propagating the auth-type query parameter
    through to the backend calls at the moment.

---
+++ frontend/helpers/api-requests.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export class RequestSender {
+    constructor(request) {
+        this.auth_type = request.query["auth-type"];
+    }
+
+    get(url, headers) {
+        return this.__request(url, "get", undefined, headers);
+    }
+
+    post(url, data, headers) {
+        return this.__request(url, "post", data, headers);
+    }
+
+    __request(url, method, data, headers) {
+        const _url = new URL(url, process.env.SERVER_BASE_URL);
+        // If the auth-type query parameter is set here it should be
+        // propagated to the backend call as well.
+        if(!_url.searchParams.has("auth-type")) {
+            _url.searchParams.set("auth-type", this.auth_type);
+        }
+        return axios({
+            method,
+            url: _url.toString(),
+            data,
+            headers,
+        })
+    }
+}
--- frontend/pages/api/fetch.js
+++ frontend/pages/api/fetch.js
@@ -1,13 +1,14 @@
-import axios from "axios";
+import { RequestSender } from "../../helpers/api-requests.js";
 
 export default async function handler(req, res) {
+    const sender = new RequestSender(req);
     return new Promise((resolve, reject) => {
         if(req.headers.authorization === undefined || req.headers.authorization === null) {
             res.status(401).json({message: "Missing authorization header"});
             return resolve();
         }
-        axios.post(`${process.env.BACKEND_URL}/api/fetch`,
-            {url: req.body.url}, {headers: {"Authorization": req.headers.authorization}})
+        sender.post(`${process.env.BACKEND_URL}/api/fetch`,
+            {url: req.body.url}, {"Authorization": req.headers.authorization})
             .then(data => {
                 res.status(data.status).send();
                 resolve();
