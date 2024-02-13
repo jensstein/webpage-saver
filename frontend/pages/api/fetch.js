f2a18ee431a350e5f0a0a6dc9e360a1e6f416c88
Jens Gwen Stein <jens.s.stein@gmail.com>
2024-02-13 12:33:14 +0100

    Raise limit for size of post body
    This request body holds the html of the pages to store.

--- frontend/pages/api/fetch.js
+++ frontend/pages/api/fetch.js
@@ -26,3 +26,15 @@ export default async function handler(req, res) {
             });
     });
 }
+
+// https://nextjs.org/docs/api-routes/request-helpers#custom-config
+export const config = {
+    api: {
+        bodyParser: {
+            // The default limit is 1mb. This is too little for some webpages.
+            // In order to avoid implementing batch uploading, I raise the limit to 10mb.
+            // next.js gives the error `413 Body exceeded 1mb limit` when too much data is posted.
+            sizeLimit: "10mb"
+        }
+    }
+}
