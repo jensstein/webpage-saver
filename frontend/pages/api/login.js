846a856d96b1312b2e21f80cd4eff58a3ab63282
Jens Gwen Stein <jens.s.stein@gmail.com>
2024-02-13 12:45:29 +0100

    Use cookie helper to set cookie on login

--- frontend/pages/api/login.js
+++ frontend/pages/api/login.js
@@ -1,19 +1,13 @@
 import axios from "axios";
 
-import nookies from "nookies";
+import { set_cookie } from "../../helpers/cookies.js";
 
 export default async function handler(req, res) {
     return new Promise((resolve, reject) => {
          axios.post(`${process.env.BACKEND_URL}/api/login`,
             {username: req.body.username, password: req.body.password})
             .then(data => {
-                nookies.set({res}, "jwt", data.data.jwt, {
-                    // If path isn't set here then its value is /api which makes it inaccessible to the other pages
-                    path: "/",
-                    httpOnly: true,
-                    secure: true,
-                    sameSite: "Strict",
-                });
+                set_cookie({res}, "jwt", data.data.jwt)
                 res.status(200).json(data.data);
                 resolve();
             })
