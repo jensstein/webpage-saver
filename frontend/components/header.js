90c63fb1a72724e1039411d6f447395c9aafcdd8
Jens Gwen Stein <jens.s.stein@gmail.com>
2024-02-25 11:23:48 +0100

    Upgrade Next.js from version 12 to 14

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -15,7 +15,7 @@ export default function Header() {
         <>
             <nav className="navbar">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active"><Link href="/"><a>Home</a></Link></li>
+                    <li className="nav-item active"><Link href="/">Home</Link></li>
                     <div onClick={logoutOnSubmit}>
                         <li className="nav-item active"><a href="">Logout</a></li>
                     </div>
