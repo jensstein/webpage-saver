fa077acbca31cb8f0d2240cfaf6c5286784e5f28
Jens Gwen Stein <jens.s.stein@gmail.com>
2024-03-09 10:26:31 +0100

    Use a mamba ui component for the article grid

--- frontend/components/links-container.js
+++ frontend/components/links-container.js
@@ -1,10 +1,15 @@
 export default function LinksContainer({children}) {
+    // Component taken from
+    // https://www.mambaui.com/components/gallery
     return (
         <>
-            <h2>Webpages</h2>
-            <div className="row">
-                {children}
-            </div>
+            <section className="py-6 dark:bg-gray-800">
+                <div className="container flex flex-col justify-center p-4 mx-auto">
+                    <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+                        {children}
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
