a9b3fd027baecb943b673b2c3e5c9faf14e8feca
Jens Gwen Stein <jens.s.stein@gmail.com>
2024-04-09 20:58:54 +0200

    Add NODE_ENV=development for development setup

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -26,7 +26,7 @@ POD_ID=$(podman pod create --replace -n $NAME-dev -p 3000:3000 -p 5000:5000 -p 5
 # because the container needs to run as user 0 to be able to start the database.
 podman create --pod $POD_ID --name $NAME-pg -e POSTGRES_DB=$NAME -e POSTGRES_PASSWORD=$NAME -e POSTGRES_USER=$NAME -u 0 -v $PWD/pg-data:/var/lib/postgresql/data:Z docker.io/postgres:14-alpine
 podman create --pod $POD_ID --workdir /home/rust/source -e CARGO_HOME=/home/rust/cargo-home -e OAUTH2_PROVIDER_BASE_URL=$OAUTH2_PROVIDER_BASE_URL -v $PWD/cargo-home-dir:/home/rust/cargo-home:Z -v $PWD/backend:/home/rust/source:Z rust-dev cargo watch -w src -w db -w Cargo.toml -x "run --bin article_server_rs -- -p 5000 --db-path postgresql://$NAME:$NAME@127.0.0.1:5432/$NAME"
-podman create --pod $POD_ID -e NEXT_TELEMETRY_DISABLED=1 -e BACKEND_URL=http://127.0.0.1:5000 -v $PWD/frontend/:/home/node/source:Z localhost/yarn yarn dev
+podman create --pod $POD_ID -e NODE_ENV=development -e NEXT_TELEMETRY_DISABLED=1 -e BACKEND_URL=http://127.0.0.1:5000 -v $PWD/frontend/:/home/node/source:Z localhost/yarn yarn dev
 
 podman pod start $POD_ID
 
